0ef598c5497be84008ae98a6a39ee7cbd533d47a
Harish Raghav <86465455+Zeus404-XD@users.noreply.github.com>
2022-11-11 00:42:30 +0530

    Update script.js
    Forgot

--- JavaScript1/script.js
+++ JavaScript1/script.js
@@ -3,11 +3,13 @@ console.log(heading)
 var array=['green','blue','red','black','orange','purple']
 let current = 0;
 const fun = () => {
+    console.log("the value of current before change is",current)
     heading.style.backgroundColor= array[current]
     current++;
     if(current==array.length){
         current = 0
     }
+    console.log("the value of current after change is",current)
 };
 const button = document.querySelector('button');
 button.addEventListener('click',fun);
